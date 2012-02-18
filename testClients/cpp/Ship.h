bb86929fb8aabbef42369ebd954212f70b07d8bd
Steven Williams <sawwx7@mst.edu>
2012-02-17 21:23:04 -0600

    Changed AI a bit. Changed mines to be based on range, not radius

---
+++ testClients/cpp/Ship.h
@@ -0,0 +1,62 @@
+// -*-c++-*-
+
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <iostream>
+#include "structures.h"
+
+class Ship;
+
+///A space ship!
+class Ship {
+  public:
+  void* ptr;
+  Ship(_Ship* ptr = NULL);
+
+  // Accessors
+  ///Unique Identifier
+  int id();
+  ///The owner of the piece
+  int owner();
+  ///Position x
+  int x();
+  ///Position y
+  int y();
+  ///ship size radius
+  int radius();
+  ///The ship type
+  char* type();
+  ///how many more attacks it has
+  int attacksLeft();
+  ///how much more movement it has
+  int movementLeft();
+  ///the largest possible movement
+  int maxMovement();
+  ///the max number of attacks it has
+  int maxAttacks();
+  ///the strength of its attacks
+  int damage();
+  ///the range of its attacks
+  int range();
+  ///the total health of the ship
+  int health();
+  ///the max health possible for the ship
+  int maxHealth();
+
+  // Actions
+  ///Command a ship to move to a specified position
+  int move(int x, int y);
+  ///Blow yourself up, damage those around you
+  int selfDestruct();
+  ///Commands your ship to attack a target
+  int attack(Ship& target);
+
+  // Properties
+
+
+  friend std::ostream& operator<<(std::ostream& stream, Ship ob);
+};
+
+#endif
+
--- testClients/williams/AI.py
+++ testClients/williams/AI.py
@@ -6,8 +6,6 @@ import random
 myShips = []
 theirShips = []
 
-def distance(fromX, toX, fromY, toY):
-  return int(math.ceil(math.sqrt((fromX-toX)**2 + (fromY-toY)**2)))
 
 class AI(BaseAI):
   """The class implementing gameplay logic."""
@@ -25,17 +23,20 @@ class AI(BaseAI):
   def end(self):
     pass
     
-    
+  def distance(self,fromX, toX, fromY, toY):
+    return int(math.ceil(math.sqrt((fromX-toX)**2 + (fromY-toY)**2)))  
+       
   def getRange(self, x1, y1, rad1, x2, y2, rad2):
     return distance(x1, x2, y1, y2) <= rad1 + rad2
     
   def moveToNearest(self,ship):
     distance = 9000; closestX = 0; closestY = 0
-    for enemy in theirShips:
-      if distance > (((ship.getX() - enemy.getX())**2) + ((ship.getY()- enemy.getY())**2))**.5:
-        distance = (((ship.getX() - enemy.getX())**2) + ((ship.getY()- enemy.getY())**2))**.5
-        closestX = enemy.getX()
-        closest = enemy.getY()
+    for enemy in ships:
+      if ship.owner != player:
+        if distance > (((ship.getX() - enemy.getX())**2) + ((ship.getY()- enemy.getY())**2))**.5:
+          distance = (((ship.getX() - enemy.getX())**2) + ((ship.getY()- enemy.getY())**2))**.5
+          closestX = enemy.getX()
+          closest = enemy.getY()
     self.moveTo(ship,closestX,closestY)
       
     
@@ -53,6 +54,8 @@ class AI(BaseAI):
       endX = int(x*distRatio)
       endY = int(y*distRatio)
       ship.move(startX + endX, startY + endY)
+
+  def attackFurthestUnit(self,ship,target):
     for enemy in self.ships:
       if ship.getAttacksLeft() >= 1 and ship.getOwner() != enemy.getOwner():
         if self.getRange(ship.getX(),ship.getY(),ship.getRange(),enemy.getX(),enemy.getY(),enemy.getRadius()):
@@ -61,7 +64,7 @@ class AI(BaseAI):
             pass
           else:
             if ship.getType() != "Mine":
-              ship.attack(enemy)          
+              ship.attack(enemy)       
 
   def run(self):
     #Find out who I am
@@ -70,19 +73,13 @@ class AI(BaseAI):
     for i in self.players:
       if i.getId() == self.playerID():
         player = i.getId() 
-    
-    del myShips[:]  
-    del theirShips[:]  
-    for ship in self.ships:
-      if ship.getOwner() == player:
-        myShips.append(ship)
-      else:
-        theirShips.append(ship)
+        
+    ships = self.ships
     
     #Locate my warp ship
     FriendlyWarpGate = []
     EnemyWarpGate = []
-    for ship in self.ships:
+    for ship in ships:
       if ship.getType() == "Warp Gate" and ship.getOwner() == player:
         FriendlyWarpGate.append(ship)
       elif ship.getType() == "Warp Gate" and ship.getOwner() != player:
@@ -111,39 +108,51 @@ class AI(BaseAI):
         if area == 0:
           shipType.warpIn(FriendlyWarpGate[0].getX()-FriendlyWarpGate[0].getRange(),FriendlyWarpGate[0].getY())
       
-      print "Trying to move enemy ships and make them attack themselves"
-      for ship in theirShips:
-        ship.move(ship.getX()+5, ship.getY()+5)
-        ship.attack(ship)
           
      
     distance = 0;distRatio = 0; startX = 0; startY = 0; endX = 0; endY = 0
     area = 1
-    modifier = random.randrange(-40,40) * (player+1)
-    for ship in myShips:
-      if len(theirShips) > 1 and len(EnemyWarpGate) > 0:
-        if ship.getType() != "Warp Gate" and ship.getType() != "Mine":
-          if area < 75:
-            if area%7 == 0:
-              self.moveTo(ship,-250+modifier,250-modifier)
-            elif area%7 == 1:
-              self.moveTo(ship,250+modifier,-250-modifier)
-            elif area%7 == 2:
-              self.moveTo(ship,0+modifier,0-modifier)
-            elif area%7 == 3:
-              self.moveTo(ship, 100+modifier,100-modifier)
-            elif area%7 == 4:
-              self.moveTo(ship,-100+modifier,-100-modifier)
-            elif area%7 == 5:
-              self.moveTo(ship,200+modifier,-100-modifier)
-            elif area%7 == 6:
-              self.moveTo(ship,-100+modifier,200-modifier)
-          else:
-            self.moveToNearest(ship)
-      else: 
-        if len(EnemyWarpGate) > 0:
-          self.moveTo(ship,EnemyWarpGate[0].getX(),EnemyWarpGate[0].getY())
-      area += 1                 
+    modifierX = random.randrange(-50,50) * (player+1)
+    modifierY = random.randrange(-50,50) * (player+1)
+    
+    #Moving my ships
+    for ship in ships:
+      if ship.getType() != "Warp Gate" and ship.getType() != "Mine" and ship.getType() != "Weapons Platform" and ship.getOwner() == player:
+        #Move weapons platform to the edge of the map
+        if area < 75:
+          if area%7 == 0:
+            self.moveTo(ship,-250+modifierX,250-modifierY)
+          elif area%7 == 1:
+            self.moveTo(ship,250+modifierX,-250-modifierY)
+          elif area%7 == 2:
+            self.moveTo(ship,0+modifierX,0-modifierY)
+          elif area%7 == 3:
+            self.moveTo(ship, 100+modifierX,100-modifierY)
+          elif area%7 == 4:
+            self.moveTo(ship,-100+modifierX,-100-modifierY)
+          elif area%7 == 5:
+            self.moveTo(ship,200+modifierX,-100-modifierY)
+          elif area%7 == 6:
+            self.moveTo(ship,-100+modifierX,200-modifierY)
+        else:
+          self.moveToNearest(ship)
+      area += 1 
+    dist = 0
+    for ship in ships:
+      furthestDist = -1
+      furthestEnemy = ships[0]
+      if ship.getOwner() == player:
+        if ship.getAttacksLeft() > 0:
+          for enemy in ships:
+            if enemy.getOwner() != player: 
+              dist = self.distance(ship.getX(),enemy.getX(),ship.getY(),enemy.getY())
+              if dist < ship.getRange() + enemy.getRadius() and dist > furthestDist:
+                furthestEnemy = enemy
+                furthestDist = dist
+          if furthestDist != -1:
+            ship.attack(furthestEnemy)
+            
+     
     return 1
 
   def __init__(self, conn):
