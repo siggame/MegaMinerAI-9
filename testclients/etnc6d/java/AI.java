33d1b2d5a7846235220629ca8880ab093e0441da
Edward Norris <etnc6d@mst.edu>
2012-04-13 21:21:08 -0500

    updated my ai

--- testclients/etnc6d/java/AI.java
+++ testclients/etnc6d/java/AI.java
@@ -6,11 +6,24 @@ import java.lang.*;
 ///The class implementing gameplay logic.
 public class AI extends BaseAI
 {
-	public static String personality = "Uno";
+	// Origin
+	// Uno
+	// Dos
+	// Tres
+	// WeaponsPlatform3
+	public static String personality = "Tres";
+	public static String persona0 = "Dos";
+	public static String persona1 = "WeaponsPlatform3";
 	public Random gen;
+	public static boolean splitPersonality = true;
 
 public String username()
 {
+	if(splitPersonality)
+		if(playerID() == 0)
+			return "AI: " + persona0;
+		else
+			return "AI: " + persona1;
 	return ("AI: " + personality);
 }
 public String password()
@@ -22,14 +35,32 @@ public String password()
 //Return true to end your turn, return false to ask the server for updated information
 public boolean run()
 {
-	if(personality == "Origin")
+	if(splitPersonality){
+		if(playerID() == 0){
+			decisionEngine(persona0);
+		}else{
+			decisionEngine(persona1);
+		}
+	}else{
+		decisionEngine(personality);
+	}
+	
+	return true;
+}
+
+public void decisionEngine(String persona){
+	if(persona.equals("Origin"))
 		aiOrigin();
-	else if(personality == "Uno")
+	else if(persona.equals("Uno"))
 		aiUno();
+	else if(persona.equals("Dos"))
+		aiDos();
+	else if(persona.equals("Tres"))
+		aiTres();
+	else if(persona.equals("WeaponsPlatform3"))
+		aiWeaponsPlatform3();
 	else
 		System.out.print("Unknown Personality");
-
-	return true;
 }
 
 //This function is called once, before your first turn
@@ -126,6 +157,119 @@ public void aiUno(){
 		}
 	}
 }
+
+public void aiDos(){
+	System.out.print("Dos: " + new Integer(turnNumber()).toString() + " " + new Integer(roundNumber()).toString() + "\n");
+	
+	while(players[playerID()].getEnergy() >= shipTypes[0].getCost()){
+		shipTypes[0].warpIn(ships[playerID()].getX(), ships[playerID()].getY());
+	}
+	
+	for(int i = 0; i < ships.length; i++){
+		if(ships[i].getOwner() == playerID()){
+			if(gen.nextFloat() < .3){
+				int k = playerID() == 0 ? 1 : 0;
+				Point loc = pointOnLine(ships[i].getX(), ships[i].getY(), ships[k].getX(),
+					ships[k].getY(), ships[i].getMaxMovement());
+				ships[i].move(loc.x, loc.y);
+			}else{
+				ships[i].move(
+					ships[i].getX() + (int)(gen.nextFloat() * ships[i].getMaxMovement()/3.0 * (gen.nextBoolean() ? 1 : -1)), 
+					ships[i].getY() + (int)(gen.nextFloat() * ships[i].getMaxMovement()/3.0 * (gen.nextBoolean() ? 1 : -1)));
+			}
+		}
+	}
+	
+	for(int i = 0; i < ships.length; i++){
+		for(int j = 0; j < ships.length; j++){
+			if(ships[i].getOwner() == this.playerID()){
+				if(ships[j].getOwner() != playerID() && 
+						distance(ships[i].getX(), ships[i].getY(), ships[j].getX(), ships[j].getY()) < ships[i].getRange()){
+					ships[i].attack(ships[j]);
+				}
+			}
+		}
+	}
+}
+
+public void aiTres(){
+	System.out.print("Tres: " + new Integer(turnNumber()).toString() + " " + new Integer(roundNumber()).toString() + "\n");
+	
+	int var = gen.nextInt() % 4;
+	while(players[playerID()].getEnergy() >= shipTypes[var].getCost()){
+		shipTypes[var].warpIn(ships[playerID()].getX(), ships[playerID()].getY());
+		var = gen.nextInt() % 4;
+	}
+	
+	for(int i = 0; i < ships.length; i++){
+		if(ships[i].getOwner() == playerID()){
+			if(gen.nextFloat() < .3){
+				int k = playerID() == 0 ? 1 : 0;
+				Point loc = pointOnLine(ships[i].getX(), ships[i].getY(), ships[k].getX(),
+					ships[k].getY(), ships[i].getMaxMovement());
+				ships[i].move(loc.x, loc.y);
+			}else{
+				ships[i].move(
+					ships[i].getX() + (int)(gen.nextFloat() * ships[i].getMaxMovement()/3.0 * (gen.nextBoolean() ? 1 : -1)), 
+					ships[i].getY() + (int)(gen.nextFloat() * ships[i].getMaxMovement()/3.0 * (gen.nextBoolean() ? 1 : -1)));
+			}
+		}
+	}
+	
+	for(int i = 0; i < ships.length; i++){
+		for(int j = 0; j < ships.length; j++){
+			if(ships[i].getOwner() == this.playerID()){
+				if(ships[j].getOwner() != playerID() && 
+						distance(ships[i].getX(), ships[i].getY(), ships[j].getX(), ships[j].getY()) < ships[i].getRange()){
+					ships[i].attack(ships[j]);
+				}
+			}
+		}
+	}
+}
+
+public void aiWeaponsPlatform3(){
+	System.out.print("WP3: " + new Integer(turnNumber()).toString() + " " + new Integer(roundNumber()).toString() + "\n");
+	int wp = -1;
+	for(int i = 0; i < 4; i++){
+		if(shipTypes[i].getType().equals("Weapons Platform"))
+			wp = i;
+	}
+	if(wp != -1){
+		while(players[playerID()].getEnergy() >= shipTypes[wp].getCost()){
+			shipTypes[wp].warpIn(ships[playerID()].getX(), ships[playerID()].getY());
+		}
+		
+		for(int i = 0; i < ships.length; i++){
+			if(ships[i].getOwner() != playerID() && ships[i].getType().equals("Weapons Platform")){
+				int opH = ships[i].getHealth();
+				int j = 0;
+				while(opH > 0){
+					if(ships[j].getOwner() == playerID()){
+						ships[j].attack(ships[i]);
+						opH -= ships[j].getDamage();
+						if(opH <= 0)
+							j--;
+					}
+					j++;
+				}
+			}
+			for(int j = 0; j < ships.length; j++){
+				if(ships[j].getOwner() == playerID())
+					ships[j].attack(ships[(playerID() == 0) ? 1 : 0]);
+			}
+		}
+	}
+	else
+		aiDos();
+}
+
+
+
+
+
+
+
 
 
 
